8af453bf29113886bcea6baae4eda7d4d583e6c4
UnkwUsr <ktoto2707043@gmail.com>
2025-04-12 01:08:39 +0300

    fix: support quicksettings tile on Android >= 14

--- android/app/src/main/kotlin/unkwusr/inbox_app/MyTileService.kt
+++ android/app/src/main/kotlin/unkwusr/inbox_app/MyTileService.kt
@@ -3,7 +3,9 @@ package unkwusr.inbox_app
 import android.service.quicksettings.TileService
 import android.service.quicksettings.Tile
 import android.content.Intent
+import android.app.PendingIntent
 import android.util.Log
+import android.os.Build
 
 class MyTileService : TileService() {
     override fun onClick() {
@@ -12,7 +14,14 @@ class MyTileService : TileService() {
                 val newIntent = MainActivity.withNewEngine().dartEntrypointArgs(listOf("launchFromQuickTile")).build(this)
 
                 newIntent.flags= Intent.FLAG_ACTIVITY_NEW_TASK
-                startActivityAndCollapse(newIntent)
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.UPSIDE_DOWN_CAKE) {
+                    startActivityAndCollapse(
+                        PendingIntent.getActivity(this, 0, newIntent, PendingIntent.FLAG_IMMUTABLE)
+                    )
+                } else {
+                    startActivityAndCollapse(newIntent)
+                }
+
             }
         catch (e:Exception){
             Log.d("debug","Exception ${e.toString()}")
